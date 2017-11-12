cf7f9a4f6cfd52652acb307addbd6bc9d9cb0d03
Maradona Morais <me@mrmorais.com.br>
2017-11-12 17:00:57 -0300

    what fuck

--- src/br/ufrn/imd/calculaarvore/Palavra.java
+++ src/br/ufrn/imd/calculaarvore/Palavra.java
@@ -37,18 +37,18 @@ public class Palavra {
 	}
 	
 	public void mesclarOcorrencias(Palavra origem) {
-		for (OcorrenciaArquivo ocorrencia_destino : ocorrencias) {
-			for (OcorrenciaArquivo ocorrencia_origem : origem.getOcorrencias()) {
+		for (OcorrenciaArquivo ocorrencia_origem : origem.getOcorrencias()) {
+			boolean newOne = true;
+			for (OcorrenciaArquivo ocorrencia_destino : ocorrencias) {
 				if (ocorrencia_destino.getArquivo().getNome().equals(ocorrencia_origem.getArquivo().getNome())) {
 					if (ocorrencia_destino.getLinha() == ocorrencia_origem.getLinha()) {
-						// Match em mesmo arquivo e mesma linha
 						ocorrencia_destino.setnRepeticoes(ocorrencia_destino.getnRepeticoes() + ocorrencia_origem.getnRepeticoes());
+						newOne = false;
 					}
-				} else {
-					// Sem match em mesmo arquivos
-					ocorrencias.add(ocorrencia_origem);
 				}
 			}
+			
+			if (newOne) ocorrencias.add(ocorrencia_origem);
 		}
 	}
 	
--- src/br/ufrn/imd/calculaarvore/test/TestArvoreTrie.java
+++ src/br/ufrn/imd/calculaarvore/test/TestArvoreTrie.java
@@ -39,4 +39,5 @@ public class TestArvoreTrie {
 			fail();
 		}
 	}
+	
 }
